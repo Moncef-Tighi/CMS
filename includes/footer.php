a18d6fb290e02dece64946747787aeb7b203ced6
Moncef-Tighi <monceftighiouart@gmail.com>
2022-01-25 19:51:35 +0100

    Léger redesign

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2014</p>
+                    <p>Copyright &copy; créé par Moncef Tighiouart </p>
                 </div>
 
             </div>
